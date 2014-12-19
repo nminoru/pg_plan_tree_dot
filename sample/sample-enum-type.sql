6f9a375badbeea80c0eba03504e3523b049aa6fb
Minoru NAKAMURA <nminoru1975@gmail.com>
2014-12-19 21:29:09 +0900

    Add a sample of enum type

---
+++ sample/sample-enum-type.sql
@@ -0,0 +1,24 @@
+CREATE EXTENSION IF NOT EXISTS pg_plan_tree_dot;
+
+CREATE TYPE color AS ENUM (
+       'black',
+       'white',
+       'red',
+       'green',
+       'blue');
+
+CREATE TABLE screen (
+       forecolor color,
+       bgcolor color);
+
+INSERT INTO screen VALUES ('red',   'black');
+INSERT INTO screen VALUES ('white', 'black');
+INSERT INTO screen VALUES ('green', 'black');
+INSERT INTO screen VALUES ('blue',  'white');
+
+-- SELECT * FROM screen WHERE bgcolor = 'black';
+SELECT generate_plan_tree_dot('SELECT * FROM screen WHERE bgcolor = ''black'';', '/tmp/sample-enum-type.dot');
+
+DROP TABLE screen;
+DROP TYPE color;
+
